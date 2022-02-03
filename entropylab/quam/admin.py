f76d7d901c0d41ec5af24c93faf1c5d4f381af57
GalW <gal@quantum-machines.co>
2022-02-03 09:43:50 +0200

    changed ConfigVar -> ConfigVars

--- entropylab/quam/admin.py
+++ entropylab/quam/admin.py
@@ -1,17 +1,17 @@
+import pickle
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum, auto
 from typing import Any
 from attr import attr
 from qualang_tools.config import ConfigBuilder
-from qualang_tools.config.parameters import ConfigVar
+
 from qualang_tools.config import components as qua_components
 from tomlkit import value
 from entropylab import LabResources, SqlAlchemyDB
 from entropylab.quam.param_store import InProcessParamStore
-from qualang_tools.config.parameters import ConfigVar
+from qualang_tools.config.parameters import ConfigVars
 from entropylab.quam.utils import DotDict
-
 
 
 def quam_init(path='.', name=None):
@@ -20,9 +20,10 @@ def quam_init(path='.', name=None):
     # quam = Quam(path,name)
     quam = None
     admin = QuamAdmin(path, name)
-    oracle = QuamOracle(path,name)
-    #oracle = None
+    oracle = QuamOracle(path, name)
+    # oracle = None
     return admin, quam, oracle
+
 
 class ParamStoreConnector:
     @staticmethod
@@ -38,15 +39,18 @@ class QuamElement(ABC):
         self.instruments = DotDict()
         super().__init__(**kwargs)
 
+
 cb_objs = ["Controller", "Transmon", "ReadoutResonator"]
 for obj in cb_objs:
-    globals()["Quam"+obj] = type("Quam"+obj, (QuamElement, getattr(qua_components, obj)), {})
+    globals()["Quam" + obj] = type("Quam" + obj, (QuamElement, getattr(qua_components, obj)), {})
+
 
 class QuamAdmin():
 
     def __init__(self, name: str = None, path='.entropy') -> None:
+        self.path = path
         self._paramStore = ParamStoreConnector.connect(path)
-        self.config_vars = ConfigVar()
+        self.config_vars = ConfigVars()
         self.elements = DotDict()
         self.config_builder_objects = DotDict()
         self.name = name
@@ -64,7 +68,7 @@ class QuamAdmin():
     def add_parameter(self, name: str, val: Any, persistent: bool = True):
         if persistent:
             self._paramStore._params[name] = val
-        self._configVar.set(name=val)
+        self.config_vars.set(name=val)
 
     def commit(self, label: str = None):
         self._paramStore.commit(label)
@@ -80,6 +84,11 @@ class QuamAdmin():
             cb.add(self.config_builder_objects[k])
         return cb.build()
 
+    def save(self):
+        with open(self.path + '/pickle.pkl', 'wb') as f:
+            pickle.dump(self, f)
+
+
 #     def add_instrument(self, name, class_name, args, kwargs):
 #         self.instruments.register_resource(name, class_name, args, kwargs)
 #         setattr(self,name,WrapperForDriver(class_name,name))
@@ -94,7 +103,7 @@ class QuamAdmin():
 # #FunctionInfo -> dictionary with instrument name, function names, parameter names
 
 class QuamOracle():
-    
+
     def __init__(self, name: str = None, path='.entropy/paramStore.db') -> None:
         self._paramStore = ParamStoreConnector.connect(path)
         self.elements = DotDict()
